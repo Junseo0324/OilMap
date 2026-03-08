1180049eab0c71d44b288e381154a6fcfd283008
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-03-08 17:57:06 +0900

    style: 바텀 네비게이션 바 디자인 스타일 적용
    - `NavigationBar`의 배경색을 흰색(`Color.White`)으로 설정
    - `NavigationBarItem`의 선택/비선택 상태에 따른 아이콘 및 텍스트 색상 적용
    - 아이템 선택 시 표시되는 인디케이터 색상을 투명(`Color.Transparent`)하게 변경하여 디자인 수정

--- app/src/main/java/com/devhjs/oilmap/core/navigation/MainBottomNavigationBar.kt
+++ app/src/main/java/com/devhjs/oilmap/core/navigation/MainBottomNavigationBar.kt
@@ -3,9 +3,12 @@ package com.devhjs.oilmap.core.navigation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import com.devhjs.oilmap.presentation.designsystem.AppColors
 
 @Composable
 fun MainBottomNavigationBar(
@@ -17,7 +20,9 @@ fun MainBottomNavigationBar(
     }
 
     if (isBottomBarVisible) {
-        NavigationBar {
+        NavigationBar(
+            containerColor = Color.White
+        ) {
             BottomNavItem.items.forEach { item ->
                 NavigationBarItem(
                     selected = selectedRoute == item.routeName,
@@ -27,7 +32,14 @@ fun MainBottomNavigationBar(
                     },
                     label = {
                         Text(text = item.title)
-                    }
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = AppColors.AlteulMain,
+                        selectedTextColor = AppColors.AlteulMain,
+                        unselectedIconColor = Color.Gray,
+                        unselectedTextColor = Color.Gray,
+                        indicatorColor = Color.Transparent
+                    )
                 )
             }
         }
