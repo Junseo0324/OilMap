03045c32f845c3214aee0aa688ec8575de6014c4
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-03-27 12:11:31 +0900

    feat: 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.devhjs.oilmap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
